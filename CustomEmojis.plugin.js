dcd57f086e644b95982d3866a8b45a0c4c8a913e
JumpOff7667 <hackingontop@gmail.com>
2020-10-04 21:12:33 +0200

    First push

---
+++ CustomEmojis.plugin.js
@@ -0,0 +1,9 @@
+/**
+ * @name CustomEmojis
+ * @version 0.0.1
+ * @description Let's you have your own emojis without the need to have a Nitro subscription
+ * 
+ * @website http://twitter.com/BandagedBD
+ */
+
+ 
